beca48e3706dc4be37ffb861ac0e96441c12348a
Asad Masroor <asadmasroor3@gmail.com>
2019-03-04 01:59:10 +0000

    Initial Commit

---
+++ Teamz2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teamz2
+//
+//  Created by Asad Masroor on 04/03/2019.
+//  Copyright © 2019 Asad Masroor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
